527af7dd2eb1ca1aecf02f5ec35bb1511bb1cd06
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2025-06-25 23:48:00 +0100

    added file and folder  structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-oxford-blue text-3xl font-bold underline">
+        hello react welcome back
+      </div>
+    </>
+  );
+}
+
+export default App;
